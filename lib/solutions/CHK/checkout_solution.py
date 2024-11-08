9ee6640398dc8e11aacb97f9e1424b18ec284e08
sbx_user1051 <sbx_user1051@169.254.136.149>
2024-11-08 22:41:46 +0000

    Fri Nov 08 22:41:46 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,29 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    items = ["A", "B", "C", "D"]
+    total = 0
+    
+    for item in skus:
+        if item not in items:
+            return -1
+    
+    for item in items:
+        count = skus.count(item)
+        if item == "A":
+            if (count - count % 3) % 3 == 0:
+                total += (count // 3) * 130
+            total += (count % 3) * 50
+        
+        if item == "B":
+            if (count - count % 2) % 2 == 0:
+                total += (count // 2) * 45
+            total += (count % 2) * 30
+
+        if item == "C":
+            total += count * 20
+
+        if item == "D":
+            total += count * 15
+
+    return total
