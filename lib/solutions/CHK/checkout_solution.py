1335e6d09ae25841dc6211852f06241b32e19bc5
sbx_user1051 <sbx_user1051@169.254.40.229>
2024-11-09 16:05:33 +0000

    Sat Nov 09 16:05:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,34 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    # items = ["A", "B", "C", "D"]
-    # total = 0
+    items = {
+        "A": {"count": 0, "value": 50}, "B": {"count": 0, "value": 30}, 
+        "C": {"count": 0, "value": 20}, "D": {"count": 0, "value": 15}, 
+        "E": {"count": 0, "value": 40}, "F": {"count": 0, "value": 10},
+        "G": {"count": 0, "value": 20}, "H": {"count": 0, "value": 10},
+        "I": {"count": 0, "value": 35}, "J": {"count": 0, "value": 60},
+        "K": {"count": 0, "value": 80}, "L": {"count": 0, "value": 90},
+        "M": {"count": 0, "value": 15}, "N": {"count": 0, "value": 40},
+        "O": {"count": 0, "value": 10}, "P": {"count": 0, "value": 50},
+        "Q": {"count": 0, "value": 30}, "R": {"count": 0, "value": 50},
+        "S": {"count": 0, "value": 30}, "T": {"count": 0, "value": 20},
+        "U": {"count": 0, "value": 40}, "V": {"count": 0, "value": 50},
+        "W": {"count": 0, "value": 20}, "X": {"count": 0, "value": 90},
+        "Y": {"count": 0, "value": 10}, "Z": {"count": 0, "value": 50},
+        }
     
-    # for item in skus:
-    #     if item not in items:
-    #         return -1
-    
-    # for item in items:
-    #     count = skus.count(item)
-    #     if item == "A":
-    #         if (count - count % 3) % 3 == 0:
-    #             total += (count // 3) * 130
-    #         total += (count % 3) * 50
-        
-    #     if item == "B":
-    #         if (count - count % 2) % 2 == 0:
-    #             total += (count // 2) * 45
-    #         total += (count % 2) * 30
-
-    #     if item == "C":
-    #         total += count * 20
-
-    #     if item == "D":
-    #         total += count * 15
-
-    # return total
-
-    items = {"A": 0, "B": 0, "C": 0, "D": 0, "E": 0, "F": 0}
     total = 0
 
     for item in skus:
@@ -49,6 +37,12 @@ def checkout(skus):
     if (items["E"] - items["E"] % 2) % 2 == 0 and items["B"] >= 1:
         items["B"] -= items["E"] // 2
 
+    if (items["N"] - items["N"] % 3) % 3 == 0 and items["M"] >= 1:
+        items["M"] -= items["N"] // 3
+
+    if (items["R"] - items["R"] % 3) % 3 == 0 and items["Q"] >= 1:
+        items["Q"] -= items["R"] // 3
+
     if items["A"] >= 5:
         total += (items["A"] // 5) * 200
         items["A"] = items["A"] % 5
@@ -56,21 +50,40 @@ def checkout(skus):
     if items["A"] >= 3:
         total += (items["A"] // 3) * 130
         items["A"] = items["A"] % 3
-    
-    total += items["A"] * 50
+
+    if items["H"] >= 10:
+        total += (items["H"] // 10) * 80
+        items["H"] = items["H"] % 10
+
+    if items["H"] >= 5:
+        total += (items["H"] // 5) * 45
+        items["H"] = items["H"] % 5
+
+    if items["V"] >= 3:
+        total += (items["V"] // 3) * 130
+        items["V"] = items["V"] % 3
+
+    if items["V"] >= 2:
+        total += (items["V"] // 2) * 90
+        items["V"] = items["V"] % 2
 
     if items["B"] >= 2:
         total += (items["B"] // 2) * 45
         items["B"] = items["B"] % 2
-    
-    total += items["B"] * 30
 
-    total += items["C"] * 20
+    if items["K"] >= 2:
+        total += (items["K"] // 2) * 150
+        items["K"] = items["K"] % 2
 
-    total += items["D"] * 15
+    if items["P"] >= 5:
+        total += (items["P"] // 5) * 200
+        items["P"] = items["P"] % 5
 
-    total += items["E"] * 40
+    if items["Q"] >= 3:
+        total += (items["Q"] // 3) * 80
+        items["Q"] = items["Q"] % 3
 
-    total += items["F"] * 10
+    for item in items.values():
+        total += item["count"] * item["value"]
 
     return total
